31f70332fbdde1679ddbedbafe693bd5eb82dc11
Neczpal Abel <neczpal@gmail.com>
2021-01-20 18:41:08 +0100

    weapon graphics added

--- js/zombie-survival.js
+++ js/zombie-survival.js
@@ -115,6 +115,7 @@ function loadCharacterImages (resourceName, frames, width, height) {
 const crateImage = loadImage ("crate", 256, 256);
 const floorImage = loadImage ("floor", 345, 345);
 const bulletImages = load4DirectionalImage ("bullet", 128, 128);
+const weaponImage = load4DirectionalImage("weapon", 74, 74);
 const zombieImages = loadCharacterImages ("zombie", 3, 74, 74);
 const heroImages = loadCharacterImages ("hero", 3, 74, 74);
 
@@ -302,6 +303,10 @@ function Player (kx, ky) {
 
     this.draw = function (mx, my) {
         ctx.drawImage (heroImages[this.walking ? anim_index : 1][this.direction], this.x - mx, this.y - my, tile_size, tile_size);
+
+        if (pressedKey[4] && this.direction !== 0) {
+            ctx.drawImage (weaponImage[this.direction], this.x - mx, this.y - my, tile_size, tile_size);
+        }
     };
 }
 
